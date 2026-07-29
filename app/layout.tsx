2592185d513d1f47a402480fbfaf247772428c34
hnbates1 <hnbates1@hotmail.com>
2026-07-29 08:31:55 -0400

    Enable enhanced traffic curve interaction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import TrafficCurveEnhancer from "./TrafficCurveEnhancer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TrafficCurveEnhancer />
         {children}
       </body>
     </html>
