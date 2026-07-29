7df144ba9da24d2a04a34c6e8ec58719f57a9339
hnbates1 <hnbates1@hotmail.com>
2026-07-29 08:55:20 -0400

    Wrap app with schedule integrity and traffic runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AppRuntime from "./AppRuntime";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,7 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRuntime>{children}</AppRuntime>
       </body>
     </html>
   );
