ba02bdeacdcb49e91f807637cafc8d90e29edbee
hnbates1 <hnbates1@hotmail.com>
2026-07-29 09:29:29 -0400

    Enable truck comments panel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppRuntime from "./AppRuntime";
+import TruckCommentsRuntime from "./TruckCommentsRuntime";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,7 +36,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AppRuntime>{children}</AppRuntime>
+        <AppRuntime>
+          {children}
+          <TruckCommentsRuntime />
+        </AppRuntime>
       </body>
     </html>
   );
